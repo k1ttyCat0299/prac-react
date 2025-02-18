d4c7e5f9df05cf1b6d279eae69450323ca1556c4
ybpark <puv299@gmail.com>
2025-02-18 16:29:28 +0900

    [env] 서버 포트번호 env참조로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+      },
     },
-  },
-});
+    server: {
+      port: Number(env.VITE_PORT) || 5173,
+    },
+    define: {
+      'process.env': env, // 소스에서는 process.env.SERVER_URL처럼 사용
+    },
+  });
+};
